8bf1db51696b25122309a87a4f847d682362ae20
Liza Daly <lizadaly@gmail.com>
2017-11-12 15:37:47 -0500

    Derp

--- screenshot.py
+++ screenshot.py
@@ -3,13 +3,18 @@ from selenium.webdriver.support.ui import WebDriverWait
 import contextlib
 import time
 
+CHAPTERS = 590
+
 
 @contextlib.contextmanager
 def quitting(thing):
     yield thing
     thing.quit()
 
+
 with quitting(webdriver.Firefox()) as driver:
-    driver.get('http://localhost:3000')
-    time.sleep(5)
-    driver.get_screenshot_as_file('images/out.png')
+    for i in range(0, CHAPTERS):
+        print("Snapping chapter {}".format(i))
+        driver.get('http://localhost:3000/?chapter={}'.format(i))
+        time.sleep(5)
+        driver.get_screenshot_as_file('images/{}.png'.format(i))
